a1efe829eedba4ae007bceb1a4e8f202676019f6
evandro.couto <evandro.coutodepaula@gmail.com>
2021-01-03 22:49:53 +0000

    Ajuste de projeto e projeto de loja

---
+++ loja/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"html/template"
+	"loja/models"
+	"net/http"
+)
+
+var temp = template.Must(template.ParseGlob("templates/*.html"))
+
+func main() {
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":8000", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	todosOsProdutos := models.BuscaTodosOsProdutos()
+
+	temp.ExecuteTemplate(w, "Index", todosOsProdutos)
+}
